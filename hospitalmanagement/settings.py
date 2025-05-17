d013ca04846d99103e8c1c960deb29ce06b38b5c
Aryamuh-ynah <humayra.hany@gmail.con>
2025-05-17 06:37:33 +0600

    Done

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -151,8 +151,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kamrunnahar.jonaki2000@gmail.com'         # ✅ your Gmail
-EMAIL_HOST_PASSWORD = 'rhvy oioy fbyv wwxg'   # ✅ App password from Gmail
+EMAIL_HOST_USER = 'your mail'         # ✅ your Gmail
+EMAIL_HOST_PASSWORD = 'PASS'   # ✅ App password from Gmail
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 TIME_ZONE = 'Asia/Dhaka'
